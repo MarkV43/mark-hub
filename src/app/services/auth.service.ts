865a79b959af327ffabe714af0d26a3f3768291f
MarkV43 <marcelinho.vogt@gmail.com>
2021-08-31 02:59:47 -0300

    Auth finally working as intended

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,17 +1,16 @@
 import {Injectable} from '@angular/core';
 import {Observable, of} from 'rxjs';
-import {User} from '../interfaces';
 import {Router} from '@angular/router';
 import {switchMap} from 'rxjs/operators';
-import {doc, docData, Firestore} from "@angular/fire/firestore";
-import {Auth, authState} from "@angular/fire/auth";
+import {doc, docData, Firestore, getDoc, setDoc} from "@angular/fire/firestore";
+import {Auth, GoogleAuthProvider, authState, signInWithPopup} from "@angular/fire/auth";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  user$: Observable<User | null | undefined>;
+  user$: Observable<any | null>;
 
   constructor(
     private auth: Auth,
@@ -22,7 +21,8 @@ export class AuthService {
     // this.user$ = this.auth.authState.pipe(
       switchMap(user => {
         if (user) {
-          return docData<User>(doc(this.afs, `users/${user.uid}`))
+          this.updateUserData(user);
+          return docData(doc(this.afs, `users/${user.uid}`))
           // return doc<User>(`users/${user.uid}`).valueChanges();
         } else {
           return of(null);
@@ -32,9 +32,10 @@ export class AuthService {
   }
 
   async googleSignIn() {
-    const credential = await this.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+    // const credential = await this.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
     // const credential = await this.auth.signInWithPopup(provider);
-    return this.updateUserData(credential);
+    const credential = await signInWithPopup(this.auth, new GoogleAuthProvider());
+    this.updateUserData(credential);
   }
 
   async signOut() {
@@ -42,16 +43,25 @@ export class AuthService {
     return this.router.navigate(['/']);
   }
 
-  private updateUserData(credential: any | null) {
+  private updateUserData(credential: any) {
     const user = credential?.user;
-    const userRef = this.afs.doc<User>(`users/${user?.uid}`);
-    const data = {
-      uid: user?.uid,
-      email: user?.email,
-      displayName: user?.displayName,
-      photoURL: user?.photoURL,
-      admin: false
-    };
-    return userRef.set(data as User, {merge: true});
+    if (!user) return;
+    const userRef = doc(this.afs, `users/${user.uid}`);
+    getDoc(userRef).then((docSnapshot) => {
+      if (!docSnapshot.exists()) {
+        console.log("Creating Document")
+        const data = {
+          uid: user.uid,
+          email: user.email,
+          displayName: user.displayName,
+          photoURL: user.photoURL,
+          admin: false
+        };
+        setDoc(userRef, data, {merge: true});
+      } else {
+        console.log("Document exists, ignoring");
+      }
+    });
+    // return userRef.set(data as User, {merge: true});
   }
 }
